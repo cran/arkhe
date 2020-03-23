0651624e6b2b97e7446fbfa98132aca24ffa0245
Nicolas Frerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2020-03-23 13:30:02 +0000

    version 0.2.2

--- inst/doc/arkhe.R
+++ inst/doc/arkhe.R
@@ -1,15 +1,46 @@
-## ----setup, include = FALSE, echo=FALSE----------------------------------
+## ----setup, include = FALSE, echo=FALSE---------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
-library(nomnoml)
 
-## ----packages------------------------------------------------------------
+## ----nomnoml, eval=FALSE, echo=FALSE------------------------------------------
+#  # Compile UML diagrams with the 'nomnoml' package
+#  nomnoml::nomnoml("
+#  [base::matrix||]
+#  [Matrix|-id: character;+dates: list;+coordinates: list|]
+#  
+#  [NumericMatrix||]
+#  [CountMatrix||]
+#  [AbundanceMatrix|+totals: numeric|]
+#  [SimilarityMatrix|+method: character|]
+#  [OccurrenceMatrix||]
+#  
+#  [LogicalMatrix||]
+#  [IncidenceMatrix||]
+#  [StratigraphicMatrix||]
+#  
+#  [base::matrix] <:- [Matrix]
+#  
+#  [Matrix] <:- [NumericMatrix]
+#  [NumericMatrix] <:- [CountMatrix]
+#  [NumericMatrix] <:- [AbundanceMatrix]
+#  [NumericMatrix] <:- [SimilarityMatrix]
+#  [NumericMatrix] <:- [OccurrenceMatrix]
+#  
+#  [Matrix] <:- [LogicalMatrix]
+#  [LogicalMatrix] <:- [IncidenceMatrix]
+#  [LogicalMatrix] <:- [StratigraphicMatrix]
+#  ", png = "vignettes/uml-1.png")
+
+## ----uml, echo=FALSE, fig.cap="UML class diagram of the S4 classes structure.", fig.width=7, fig.height=7, out.width="100%"----
+knitr::include_graphics("uml-1.png", error = TRUE)
+
+## ----packages-----------------------------------------------------------------
 # Load packages
 library(arkhe)
 
-## ----create--------------------------------------------------------------
+## ----create-------------------------------------------------------------------
 set.seed(12345)
 ## Create a count data matrix
 CountMatrix(data = sample(0:10, 100, TRUE),
@@ -20,7 +51,7 @@ CountMatrix(data = sample(0:10, 100, TRUE),
 IncidenceMatrix(data = sample(0:1, 100, TRUE),
                 nrow = 10, ncol = 10)
 
-## ----coerce--------------------------------------------------------------
+## ----coerce-------------------------------------------------------------------
 ## Create a count matrix
 A0 <- matrix(data = sample(0:10, 100, TRUE), nrow = 10, ncol = 10)
 
